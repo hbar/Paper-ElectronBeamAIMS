402bd3dc69e36183dd6aeee871fcb7448617b8de
hbar <haroldbarnard@gmail.com>
2015-01-15 12:14:19 -0500

    added figures and calcuations with substantial work on the text

--- calculations/SolidAngleCalculations.py
+++ calculations/SolidAngleCalculations.py
@@ -8,9 +8,10 @@ Adet = (25e-3)**2
 
 R = linspace(0.01,10,1000)
 
-BK1000nm = 2000.26 * 19.7e-2
-MoK = 2561.99
-MoL = 4103.36
+BK1000nm = 268.26 * 19.7e-2 *100
+MoK = 2635.07
+MoL = 4071.73
+CrK = 8863.95
 
 Omega0 = Adet/R**2
 
@@ -19,16 +20,19 @@ pl.loglog(R,Omega0)
 pl.xlabel(r'X-Ray Path Length $R$ [m]')
 pl.ylabel(r'Solid Angle $\Omega$ [Sr]')
 
-LabelBK1000nm = r'B(1$\mu$m)-$K_{\alpha}$ ($0.18$ keV)'
-LabelK = r'Mo-$K_{\alpha}$ ($17.4$ keV)'
-LabelL = r'Mo-$L$ ($2.3$ keV)'
+LabelBK100nm = r'B(100nm)-$K_{\alpha}$ [$0.18$ keV]'
+LabelK = r'Mo-$K_{\alpha}$ [$17.4$ keV]'
+LabelL = r'Mo-$L$ [$2.3$ keV]'
+LabelCrK = r'Cr-$K$ (under Mo(500nm)) [6.0 keV]'
 
 pl.figure()
-pl.loglog(R,BK1000nm*Omega0*1000,label=LabelBK1000nm)
-pl.loglog(R,MoK*Omega0*1000,label=LabelK)
+pl.loglog(R,CrK*Omega0*1000,'k',label=LabelCrK)
+pl.loglog(R,BK1000nm*Omega0*1000,label=LabelBK100nm)
 pl.loglog(R,MoL*Omega0*1000,'r',label=LabelL)
+pl.loglog(R,MoK*Omega0*1000,label=LabelK)
 pl.xlabel(r'X-Ray Path Length $R$ [m]')
 pl.ylabel(r'Detected X-Rays [Photons/$\mu$C]')
+pl.title('Detected X-Rays for 25mm$^2$ Amptek SDD detector')  
 pl.grid()
 pl.legend()
 
